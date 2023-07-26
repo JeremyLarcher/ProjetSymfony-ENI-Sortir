c55f8aee8fdc0e482d54f1857b870cf023976b6a
JeremyLarcher <larcherjeremy@hotmail.fr>
2023-07-26 16:29:06 +0200

    amélioration formulaire sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -19,11 +19,27 @@ class SortieController extends AbstractController
      */
     public function creersortie(Request $request)
     {
+
+
         $sortie = new Sortie();
+
         $SortiesType = $this->createForm(SortiesType::class, $sortie);
+
 
         return $this->render('creersortie.html.twig',[
             'SortiesType'=>$SortiesType->createView()
         ]);
     }
 }
+
+
+/* to do:   ajouter l'enregistrement des données de sortie créé en BDD
+            Le campus organisateur s'ajoute automatiquement en fonction du campus associé à l'utilisateur et s'affiche dans un champ sans pouvoir être changé, j'ai passé 2h dessus trop chaud
+            la sélection du lieu se fait sous forme d'un menu déroulant en fonction de ce qui est dans la BDD, rue et code postal vienne s'insérer automatiquement suite au choix fait
+            Ajouter la possibilité de créé un nouveau lieu si celui souhaité n'est pas dans la liste.
+            Ajouter le status "crée" automatiquement à la création d'une sortie (il sera modifiable plus tard par l'organisateur)
+            Ajouter automatiquement à la création de la sortie l'id de l'organisateur dans le champ organisateur id
+            C'est fait : La durée se donne en minute sous forme de chiffre (pas de string)
+            C'est fait : La description de sortie et sous forme de textarea pour voir ce que l'on écrit dedans.
+            Le fichier twig lié au sortie doit être dans le dossier sortie dans template
+*/
--- src/Form/SortiesType.php
+++ src/Form/SortiesType.php
@@ -2,10 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,6 +19,7 @@ class SortiesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
         $builder
             ->add('nom', TextType::class, [
                 'label' => 'Nom de la sortie :',
@@ -36,14 +41,22 @@ class SortiesType extends AbstractType
                 'label' => 'Nombre de places :',
                 'required' => true
             ])
-            ->add('duree', NumberType::class, [
-                'label' => 'DurÃ©e maximum de la sortie (en heures) :',
+            ->add('duree', IntegerType::class, [
+                'label' => 'Durée de la sortie (en minute) :',
+                'attr' => [
+                    'inputmode' => 'numeric',
+                    'step' => 1,
+                ],
                 'required' => false
             ])
-            ->add('infosSortie', TextType::class, [
+            ->add('infosSortie', TextareaType::class, [
                 'label' => 'Description et informations :',
+                'attr' => [
+                    'rows' => 3,
+                ],
                 'required' => true
             ])
+
             ->add('lieu', TextType::class, [
                 'label' => 'Lieu de la sortie :',
                 'required' => false
@@ -55,7 +68,9 @@ class SortiesType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+
             'data_class' => Sortie::class,
         ]);
+
     }
 }
