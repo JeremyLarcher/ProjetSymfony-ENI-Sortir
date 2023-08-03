59be07a02fbca9b784c6494571f42fa39aad5c90
LeQueauT <thibaud.le_queau@yahoo.fr>
2023-08-03 11:27:58 +0200

    Modifier le profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\ModifierProfilType;
+use App\Form\ParticipantType;
 use App\Repository\ParticipantRepository;
 use App\Entity\Participant;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,54 +21,74 @@ use Symfony\Component\HttpFoundation\Request;
 
 
 
-class ProfilController extends AbstractController
+    class ProfilController extends AbstractController
 
-
-{
-    /**
-     * @Route("/detail", name="detail")
-     */
-    public function profil(): Response
     {
-        //*       $participant = $participantRepository->findOneBy($this->getUser()->getId());
-        $user = $this->getUser();
+        /**
+         * @Route("/detail", name="detail")
+         */
+        public function profil(): Response
+        {
+            //*       $participant = $participantRepository->findOneBy($this->getUser()->getId());
+            $user = $this->getUser();
 
-        return $this->render('main/profil/profil.html.twig', [
-            "participant" => $user
-        ]);
-    }
+            return $this->render('main/profil/profil.html.twig', [
+                "participant" => $user
+            ]);
+        }
 
-    /**
-     * @Route("/modifier", name="modifier")
-     */
-    public function modifier(): Response
-    {
-        //*       $participant = $participantRepository->findOneBy($this->getUser()->getId());
-        $user = $this->getUser();
+        /**
+         * @Route("/modifier", name="modifier"), methods={"GET", "POST"})
+         */
+        public function modifier(Request $request, EntityManagerInterface $entityManager, ParticipantRepository $participantRepository): Response
 
-        return $this->render('main/profil/profil.html.twig', [
-            "participant" => $user
-        ]);
-    }
+            {
+                $user = $this->getUser(); // Obtient l'utilisateur connecté
 
-    /**
-     * @Route("/{id}/delete", name="delete", methods={"GET"})
-     */
-    public function delete(Request $request, int $id, ParticipantRepository $participantRepository): Response
-    {
-            $participant=$participantRepository->find($id);
+            // Crée un formulaire en utilisant le FormBuilder
+
+            $form = $this->createForm(ModifierProfilType::class, $user);
+
+            // Traite la soumission du formulaire
+
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid())
+            {
+                // Enregistre les modifications dans la base de données
+                $entityManager->flush();
+
+                // Redirige vers une page de confirmation ou ailleurs
+                return $this->redirectToRoute('profil_detail');
+            }
+
+            return $this->render('main/profil/editprofil.html.twig', [
+                'ModifierProfil' => $form->createView(),
+            ]);
+
+        }
+
+
+        /**
+         * @Route("/{id}/delete", name="delete", methods={"GET"})
+         */
+        public function delete(Request $request, int $id, ParticipantRepository $participantRepository): Response
+        {
+            $participant = $participantRepository->find($id);
 
             $participantRepository->remove($participant, true);
 
             $this->addFlash('sup', 'Votre compte a bien été supprimée');
 
+            return $this->render('main/accueil.html.twig', [
 
-
-
-        return $this->render('main/accueil.html.twig', [
-
-        ]);
+            ]);
+        }
     }
-}
+
+
+
+
+
+
 
 
